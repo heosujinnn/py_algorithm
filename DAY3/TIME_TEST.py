ee2e34b33670c53e61824b9f776676a736407514
허수진 <0204sujin@naver.com>
2022-06-29 02:33:15 +0000

    그리디 알고리즘 - 거스름 돈

--- DAY3/TIME_TEST.py
+++ DAY3/TIME_TEST.py
@@ -1,3 +1,4 @@
+#2중 반복문을 이용하는 프로그램 예제, N = 2000 기준, 시간 계산
 import time 
 import random 
 
@@ -8,7 +9,8 @@ for v in range(2000):
 for i in array:
     for j in array:
         temp=i*j 
-        #print(temp)
+    
 print(temp)
 end_time=time.time()
 print("time:",end_time-start_time)
+
